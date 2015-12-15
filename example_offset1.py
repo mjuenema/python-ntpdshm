bb8cc128c3c35f928f71ac1cee4700adf7a28334
Markus Juenemann <markus@juenemann.net>
2015-12-16 10:16:34 +1100

    Completed example_offset1.py

--- example_offset1.py
+++ example_offset1.py
@@ -6,11 +6,9 @@ Just for fun example as described in README.rst.
 Markus Juenemann, 12-Dec-2015
 """
 
-
-
 import time
-import ntpdshm
 import struct
+import ntpdshm
 
 def get_clock_time():
     return time.time() - 1.0     # always be exactly one second behind.
@@ -26,7 +24,7 @@ def main():
         ntpd_shm.update(clock_time)
         print clock_time, ntpd_shm.count
         #print struct.unpack('iiiiiiiiiiII8i', ntpd_shm.shm.read())
-        print len(ntpd_shm.shm.read())
+        #print len(ntpd_shm.shm.read())
         time.sleep(1.0)
 
 if __name__ == '__main__':
